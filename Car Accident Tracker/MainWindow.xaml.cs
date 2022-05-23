e1fe512d99bfa42c035ddd48a1944346419cc27c
Hamza Elshennawy <h.elshennawy@nu.edu.eg>
2022-05-23 14:54:43 +0200

    Search Added

--- Car Accident Tracker/MainWindow.xaml.cs
+++ Car Accident Tracker/MainWindow.xaml.cs
@@ -31,11 +31,14 @@ namespace Car_Accident_Tracker
             ReadData();
             DisplayFirstTenRows();
             ReadDrivers();
+            ReadLocations();
+            ReadAccidentsNumbers();
         }
 
         List<ObjectToDisplay> AllRecords = new List<ObjectToDisplay>();
         List<Driver> DriversNames = new List<Driver>();
         List<Accident> AllAcc = new List<Accident>();
+        List<string> AllLocations = new List<string>();
         private void DisplayAllBTN_Click(object sender, RoutedEventArgs e)
         {
             ReadData();
@@ -50,13 +53,15 @@ namespace Car_Accident_Tracker
 
         private void SearchBTN_Click(object sender, RoutedEventArgs e)
         {
-
+            SearchByLocation();
+            //SearchByDriverName();
+            //SearchByAccidentNumber();
         }
 
         public void LoadAllAccidents()
         {
-            AccLocation_CB.Items.Clear();
-            AccLocation_CB.Items.Add("Cairo");
+            AccLocation_TB.Text = "";
+
         }
         
         public void DisplayFirstTenRows()
@@ -116,11 +121,122 @@ namespace Car_Accident_Tracker
                 if(!Names.Contains(TempDriverName))
                 {
                     Names.Add(TempDriverName);
-                    DriverName_CB.Items.Add(TempDriverName);
+                    //DriverName_CB.Items.Add(TempDriverName);
                 }
             }
             DriversNames.Add(TempDriver);
-            
+            //DriverName_CB.ItemsSource = Names;
         }
+
+        private void AccNumber_CB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void DriverName_CB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //DriverName_TB.Text = DriversNames[0];
+        }
+
+        public void ReadLocations()
+        {
+            Driver TempDriver = new();
+            Accident TempAcc = new(TempDriver);
+            string TempLocation;
+            List<string> Locations = new List<string>();
+
+
+            for (int i = 0; i < AllRecords.Count; i++)
+            {
+                TempAcc = AllAcc[i];
+                TempLocation = TempAcc.getAccidentLocation();
+                if (!Locations.Contains(TempLocation))
+                {
+                    Locations.Add(TempLocation);
+                    //DriverName_CB.Items.Add(TempDriverName);
+                }
+            }
+            //AccLocation_TB.Text = Locations;
+        }
+        public void ReadAccidentsNumbers()
+        {
+            Driver TempDriver = new();
+            Accident TempAcc = new(TempDriver);
+            string TempAccidentNumber;
+            List<string> Accidents = new List<string>();
+
+
+            for (int i = 0; i < AllRecords.Count; i++)
+            {
+                TempAcc = AllAcc[i];
+                TempAccidentNumber = TempAcc.getAccidentNumber();
+                if (!Accidents.Contains(TempAccidentNumber))
+                {
+                    Accidents.Add(TempAccidentNumber);
+                    //DriverName_CB.Items.Add(TempDriverName);
+                }
+            }
+            //AccNumber_TB.Text = Accidents;
+        }
+        public void SearchByLocation()
+        {
+            string LocationName = AccLocation_TB.Text;
+
+            Driver TempDriver = new();
+            Accident TempAcc = new Accident(TempDriver);
+            List<Accident> TempAccLocations_Obj = new List<Accident>();
+            DataGridXaml.Items.Clear();
+            for(int i=0;i<AllRecords.Count;i++)
+            {
+                TempAcc = AllAcc[i];
+                if(TempAcc.getAccidentLocation() == LocationName)
+                {
+                    TempAccLocations_Obj.Add(TempAcc);
+                    ObjectToDisplay TempDisplayObj = new ObjectToDisplay(TempAcc);
+                    DataGridXaml.Items.Add(TempDisplayObj);
+                }
+            }
+        }
+        public void SearchByDriverName()
+        {
+            string Driver_Name = DriverName_TB.Text;
+
+            Driver TempDriver = new();
+            Accident TempAcc = new Accident(TempDriver);
+            List<Accident> TempDriverName_Obj = new List<Accident>();
+            DataGridXaml.Items.Clear();
+            for (int i = 0; i < AllRecords.Count; i++)
+            {
+                TempAcc = AllAcc[i];
+                if (TempAcc.getAccidentDriver().DriverName == Driver_Name)
+                {
+                    TempDriverName_Obj.Add(TempAcc);
+                    ObjectToDisplay TempDisplayObj = new ObjectToDisplay(TempAcc);
+                    DataGridXaml.Items.Add(TempDisplayObj);
+                }
+            }
+        }
+
+        public void SearchByAccidentNumber()
+        {
+            string Accident_Number = AccNumber_TB.Text;
+
+            Driver TempDriver = new();
+            Accident TempAcc = new Accident(TempDriver);
+            //List<Accident> TempDriverName_Obj = new List<Accident>();
+            DataGridXaml.Items.Clear();
+            for (int i = 0; i < AllRecords.Count; i++)
+            {
+                TempAcc = AllAcc[i];
+                if (TempAcc.getAccidentNumber() == Accident_Number)
+                {
+                    //TempDriverName_Obj.Add(TempAcc);
+                    ObjectToDisplay TempDisplayObj = new ObjectToDisplay(TempAcc);
+                    DataGridXaml.Items.Add(TempDisplayObj);
+                    break;
+                }
+            }
+        }
+
     }
 }
